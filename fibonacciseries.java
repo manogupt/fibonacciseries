d24fb9540a3de12856bea548bbe8e12aa6353fc5
manogupt <manojgupta123929@gmail.com>
2022-06-02 19:00:46 +0530

    Added : Fibonacciseries program

---
+++ fibonacciseries.java
@@ -0,0 +1,22 @@
+
+public class fibonacciseries {
+
+
+	public static void main(String[] args) {
+		
+		int n = 10, firstTerm = 0, secondTerm = 1;
+		System.out.println("Fibonacci Series till " + n  + " terms:");
+		
+			for (int i=1; i <= n; i++){
+				System.out.println(firstTerm + " ");
+				
+				int nextTerm = firstTerm + secondTerm;
+				firstTerm = secondTerm;
+				secondTerm = nextTerm;
+		}
+
+	}
+
+}
+
+
