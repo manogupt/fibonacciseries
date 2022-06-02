4790ead70183a4e8e63a26603964be453fed6d0c
manogupt <manojgupta123929@gmail.com>
2022-06-02 19:17:28 +0530

    Added : QuotientRemainder program

---
+++ QuotientRemainder.java
@@ -0,0 +1,21 @@
+
+public class QuotientRemainder {
+
+		public static void main(String[] args) {
+			
+			int dividend = 25, divisor = 4;
+			int quotient, remainder;
+			
+			System.out.println("Dividend = " + dividend);
+			System.out.println("Divisor = " + divisor);
+			
+			quotient = dividend / divisor;
+			System.out.println("Quotient = " + quotient);
+			
+			remainder = dividend % divisor;
+			System.out.println("Remainder = " + remainder);
+			
+		}
+
+	}
+
